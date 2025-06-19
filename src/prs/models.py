3e3f9250637ce571c27b5426356cad25d683e3f9
PBreno <brenopombobp@hotmail.com>
2025-06-18 23:00:59 -0300

    create the file and read the json file

--- src/prs/models.py
+++ src/prs/models.py
@@ -1,3 +1,36 @@
+import json
+from pathlib import Path
+
 from django.db import models
 
 # Create your models here.
+BASE_DIR = Path(__file__).resolve().parent.parent
+def create_file(data):
+
+    path = BASE_DIR /'data.json'
+    if not data:
+        return {
+            'message': 'File not found.',
+        'data': None}
+
+    try:
+        with open(path, "w") as file:
+            json.dump(data, file, indent=4)
+
+    except FileNotFoundError as e:
+        print(e)
+        return None
+
+def read_file():
+
+    try:
+
+        with open(BASE_DIR / 'data.json', 'r') as file:
+            json_data = json.load(file)
+            return json_data
+    except FileNotFoundError as e:
+        return e
+class Files:
+
+    def read(self):
+        return 0
--- src/prs/views.py
+++ src/prs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import os.path
 
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import *
 # Create your views here.
+
+def index(request):
+
+    #create_file(data)
+    players_registered = read_file()
+    #paginator = Paginator(players_registered, 10)
+    #page_number = request.GET.get('page')
+    #page = paginator.get_page(page_number)
+
+    context = {
+        'page': players_registered,
+        'site_title': 'Player Registration System',
+    }
+
+    return render(request, 'prs/index.html', context)
